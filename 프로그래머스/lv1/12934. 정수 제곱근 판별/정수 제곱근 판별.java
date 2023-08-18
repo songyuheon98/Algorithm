57f7a448a4e5f01b0052d1296cdfd47e238c0113
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-18 13:58:41 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,14 +1,6 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long num=0;
-        while(n>(num*num)){
-            num++;
-            if(n==num*num){
-                answer=(num+1)*(num+1);
-                return answer;
-            }
-        }
-        return -1;
+        return answer;
     }
 }
