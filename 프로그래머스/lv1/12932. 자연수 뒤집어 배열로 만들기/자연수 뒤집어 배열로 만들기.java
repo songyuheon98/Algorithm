6fd7258bd1cc17bb2d6946b27dda3302d239a9c4
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-19 20:15:27 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 15.51 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,35 +1,20 @@
-import java.util.*;
+import java.util.stream.IntStream;
 class Solution {
+            public int[] solution(long n) {
+                long temp=n;
 
-/*
-* 자연수 뒤집어 배열로 만들기
-문제 설명
-자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.*/
+                String result = "";
+                System.out.println("----");
+                while(temp!=0){
+                    result+=temp%10;
+                    temp /= 10;
+                }
 
-    // input
-    // 자연수 n
-    //
-    // 계산
-    // 자연수 n을 뒤집기 reverse()
-    // 숫자->문자열  "".toString or String.valueOf()
-    // 뒤집은거 배열화 필요
-    //
-    // output
-    // 자연수 n을 뒤집어 각자리 숫자를 원소로 가지는 배열
-    public int[] solution(long n) {
-            int[] answer;
-            ArrayList<Integer> arrInt = new ArrayList<>();
+                char []temp_result =result.toCharArray();
+                int [] answer = IntStream.range(0,temp_result.length)
+                        .map(i-> Integer.parseInt(String.valueOf(temp_result[i])))
+                        .toArray();
 
-            String transStr = String.valueOf(n); //숫자n을 문자열로 변경
-            StringBuilder strReverse = new StringBuilder(transStr); //reverse 사용하기 위해 생성
-            strReverse = strReverse.reverse();//문자열n을 뒤집기
-            String temp = strReverse.toString();
-            for (int i = 0; i < strReverse.length(); i++) {
-                arrInt.add(Integer.parseInt(String.valueOf(temp.charAt(i))));
+                return answer;
             }
-
-            return arrInt.stream().mapToInt(i->i).toArray();
-
-
-    }
-}
+        }
