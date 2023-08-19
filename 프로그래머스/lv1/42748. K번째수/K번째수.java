9e7d2c849860374e033eb0e1bd0778436823077c
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-19 23:33:37 +0900

    [level 1] Title: K번째수, Time: 0.55 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,18 +1,30 @@
 import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0] - 1;
-            int end = commands[i][1];
-            int k = commands[i][2] - 1;
 
-            int[] temp = Arrays.copyOfRange(array, start, end);
+        for (int i = 0; i < commands.length; i++) {
+            int [] temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
             Arrays.sort(temp);
-            answer[i] = temp[k];
+            answer[i]=temp[commands[i][2]-1];
         }
         return answer;
     }
-}
+}    
+//class Solution {  // Sub string 이용
+//    public int[] solution(int[] array, int[][] commands) {
+//        int[] answer = new int[commands.length];
+//        String result="";
+//        for (int i = 0; i < array.length; i++)
+//            result+=array[i];
+//        //sol sub string하고 for문 쓰면 될 듯
+//        for (int i = 0; i < commands.length; i++) {
+//            String temp =(result.substring(commands[i][0]-1, commands[i][1]));
+//            char [] tempChar = temp.toCharArray();
+//            Arrays.sort(tempChar);
+//            temp =String.valueOf(tempChar);
+//            answer[i]=Integer.parseInt(temp.substring(commands[i][2]-1,commands[i][2]));
+//        }
+//        return answer;
+//    }
+//}
