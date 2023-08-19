4d2d73f18645a503d256aaf9355241c984d29aeb
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-19 15:45:22 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,24 +1,12 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        // sol for문 3개 사용
-        //0 1 2
-        // 0 1 3
-        // 0 1 4
-        // 1 2 3
-        // 1 2 4
-        // 2 3 4
 
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i+1; j < number.length; j++) {
-                for (int k = j+1; k < number.length; k++) {
+        for (int i = 0; i < number.length; i++) 
+            for (int j = i+1; j < number.length; j++) 
+                for (int k = j+1; k < number.length; k++) 
                     if((number[i]+number[j]+number[k])==0)
                         answer+=1;
-                }
-            }
-        }
-        
-        
         
         return answer;
     }
