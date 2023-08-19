f29d448f49e572b190cc6c2be389c1aa0ef2885a
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-19 16:22:26 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,13 +1,26 @@
 class Solution {
+    public int dfs(int start,int len,int count,int []number,int sum,int[] temp){
+        if(count==2) {
+            if (sum == 0) {
+                temp[0]+=1;
+                return 1;
+            }
+            return 0;
+        }
+        for (int i = start; i < len; i++) {
+            int sum_dfs = sum;
+            sum_dfs += number[i];
+            dfs(i + 1, len, count + 1, number, sum_dfs,temp);
+        }
+        return 0;
+    }
     public int solution(int[] number) {
-        int answer = 0;
-
-        for (int i = 0; i < number.length; i++) 
-            for (int j = i+1; j < number.length; j++) 
-                for (int k = j+1; k < number.length; k++) 
-                    if((number[i]+number[j]+number[k])==0)
-                        answer+=1;
+        int [] temp = {0};
+        int answer=0,len =number.length,result=0;
         
-        return answer;
+        for (int i = 0; i < len; i++) 
+            dfs(i+1,len,0,number,number[i],temp);
+        
+        return temp[0];
     }
 }
