89d26459d9e6c761b891199ba857338e2a5f981c
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-19 12:58:09 +0900

    [level 1] Title: 행렬의 덧셈, Time: 55.88 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,12 +1,17 @@
 import java.util.*;
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-      int len = arr1.length,len2=arr1[0].length;
-        int [][]answer=new int[len][len2];
+        public int[][] solution(int[][] arr1, int[][] arr2) {
+        // sol for 문 중첩으로 문제 해결 + ArrayList 2차원 배열
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        int len = arr1.length,len2=arr1[0].length;
 
-        for (int i = 0; i < len; i++)
-            for (int j = 0; j < len2; j++)
-                answer[i][j]=arr1[i][j]+arr2[i][j];
-        return answer;
-    }
+        for (int i = 0; i < len; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < len2; j++) {
+                row.add(arr1[i][j]+arr2[i][j]);
+            }
+            result.add(row);
+        }
+        return result.stream().map(sub->sub.stream().mapToInt(Integer::intValue).toArray()).toArray(int[][]::new);
+}
 }
