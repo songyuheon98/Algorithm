d1a25806a99cdca551933a8a8ad20901c5de6549
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-23 18:22:50 +0900

    [unrated] Title: 명예의 전당 (1), Time: 8.41 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        LinkedList<Integer> temp = new LinkedList<>();
+        int[] answer = new int[score.length];
+        for (int i = 0; i < score.length; i++) {
+            if(temp.size()<k) {
+                temp.add(score[i]);
+                answer[i]= (temp.stream().mapToInt(Integer::intValue).min().getAsInt());
+                continue;
+            }
+            int min =temp.stream().mapToInt(Integer::intValue).min().getAsInt();
+                // min 값을 쓰기 위해서는 maptoint를 통해 int형으로 바꾸어 줘야한다. Stream<Integer> -> IntStream
+
+            if(temp.size()>=k && score[i]>=min){
+                temp.remove((Integer) min);
+                temp.add(score[i]);
+            }
+            answer[i]= (temp.stream().mapToInt(Integer::intValue).min().getAsInt());
+        }
+        for (int num:answer) {
+            System.out.print(num + " ");
+        }
+        return answer;
+    }
+}
