2ab1d1ae248dfed263b7b8200bc5cee5a18efc37
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-23 16:21:47 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 16.20 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,16 +1,30 @@
+// class Solution {
+//     // 설계
+//     // 재귀 함수 이용
+//     // int 음식 종류, int []
+//     public String recursion(int [] food, int food_name){
+//         String returnString ="";
+//         if(food_name==food.length) 
+//             return "0";
+//         for (int i = 0; i < food[food_name]/2; i++) 
+//             returnString += food_name;
+//         return returnString += (recursion(food, food_name + 1) +returnString);
+//     }
+//     public String solution(int[] food) {
+//         return recursion(food,1);
+//     }
+// }
+
 class Solution {
-    // 설계
-    // 재귀 함수 이용
-    // int 음식 종류, int []
-    public String recursion(int [] food, int food_name){
-        String returnString ="";
-        if(food_name==food.length) 
-            return "0";
-        for (int i = 0; i < food[food_name]/2; i++) 
-            returnString += food_name;
-        return returnString += (recursion(food, food_name + 1) +returnString);
-    }
     public String solution(int[] food) {
-        return recursion(food,1);
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
+            }
+        }
+
+        return answer;
     }
 }
