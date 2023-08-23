c496b83025247389cbfa3f3e06012df3f4eac010
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-23 15:47:54 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 94.89 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,21 +1,33 @@
 import java.util.Arrays;
+// class Solution {
+//     public int[] solution(String s) {
+//         int len =s.length(),check =0;
+//         int [] answer = new int[len];
+//         Arrays.fill(answer,-1);
+
+//         char [] array = s.toCharArray();
+//         for (int i = 0; i < len; i++)
+//             for (int j = i-1; j >= 0 ; j--)
+//                 if(array[j]==array[i]) {
+//                     answer[i] = i - j;
+//                     break;
+//                 }
+
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(String s) {
-        int len =s.length(),check =0;
+        int len =s.length();
         int [] answer = new int[len];
         Arrays.fill(answer,-1);
 
-        char [] array = s.toCharArray();
         for (int i = 0; i < len; i++)
-            for (int j = i-1; j >= 0 ; j--)
-                if(array[j]==array[i]) {
-                    answer[i] = i - j;
-                    break;
-                }
-        for ( int a: answer) {
+            answer[i] = (s.substring(0,i).lastIndexOf(s.substring(i,i+1))!=-1)?i-s.substring(0,i).lastIndexOf(s.substring(i,i+1)):-1;
+
+        for ( int a: answer)
             System.out.print(a+" ");
-        }
-        System.out.println();
         return answer;
     }
 }
