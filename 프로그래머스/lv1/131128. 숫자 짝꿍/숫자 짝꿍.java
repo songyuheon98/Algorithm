3d455a36a8b11c65c98200c28e866f32ae1ec3db
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-09-02 09:25:33 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.69 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -3,31 +3,27 @@ import java.util.PriorityQueue;
 
 class Solution {
     public String solution(String X, String Y) {
-        // 문자 빈도를 저장하기 위한 HashMap을 생성합니다.
-        HashMap<Character, Integer> yFrequency = new HashMap<>();
-
-        // Y의 문자 빈도를 기록합니다.
+        
+        HashMap<Character, Integer> yFreq = new HashMap<>();
+        
         for (char ch : Y.toCharArray()) {
-            yFrequency.put(ch, yFrequency.getOrDefault(ch, 0) + 1);
-        }
-
-        // 우선순위 큐 (최대 힙)를 사용하여 문자를 내림차순으로 정렬합니다.
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-
-        // X의 문자 중 Y에 있는 문자만 maxHeap에 추가합니다.
-        for (char ch : X.toCharArray()) {
-            if (yFrequency.getOrDefault(ch, 0) > 0) {
-                maxHeap.add(ch);
-                yFrequency.put(ch, yFrequency.get(ch) - 1);
-            }
-        }
-
-        // StringBuilder를 사용하여 결과 문자열을 구성합니다.
-        StringBuilder sb = new StringBuilder();
-        while (!maxHeap.isEmpty()) {
-            sb.append(maxHeap.poll());
+            yFreq.put(ch, yFreq.getOrDefault(ch, 0) + 1);
         }
         
+        PriorityQueue<Character> priQueue = new PriorityQueue<>((a, b) -> b - a);
+        
+        for (char ch : X.toCharArray()) {
+            if (yFreq.getOrDefault(ch, 0) > 0) {
+                priQueue.add(ch);
+                yFreq.put(ch, yFreq.get(ch) - 1);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!priQueue.isEmpty()) {
+            sb.append(priQueue.poll());
+        }
+
         String answer = sb.toString();
 
         if (answer.isEmpty())
@@ -35,5 +31,6 @@ class Solution {
         else if (answer.replace("0", "").isEmpty())
             return "0";
         return answer;
+        
     }
 }
