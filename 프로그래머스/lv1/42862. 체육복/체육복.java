df1c71c958433d8a64b1dc0d92eaf77ee7d17d6b
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-09-04 09:31:22 +0900

    [level 1] Title: 체육복, Time: 2.67 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        lost= Arrays.stream(lost).sorted().toArray();
+        reserve= Arrays.stream(reserve).sorted().toArray();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : reserve)
+            map.put(num, 2);
+
+        for ( int num: lost)
+            if (map.containsKey(num)) {
+                map.replace(num, 1);
+                n++;
+            }
+
+
+
+        for (int num : lost) {
+            if (map.containsKey(num - 1) && map.get(num - 1) == 2) {
+                map.put(num, 1);
+                map.replace(num - 1, 1);
+            } else if (map.containsKey(num + 1) && map.get(num + 1) == 2) {
+                map.put(num, 1);
+                map.replace(num + 1, 1);
+            }
+
+        }
+
+        for (int i = 0; i < map.size(); i++)
+            answer++;
+        return answer+n-lost.length-reserve.length;
+    }
+
+}
